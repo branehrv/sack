dbb0325cb062d0b0b5409290d50fb651ceab7a71
branehrv <68347743+branehrv@users.noreply.github.com>
2020-08-19 11:22:14 +0200

    Update tretje število po velikosti.py

--- Vaje/tretje število po velikosti.py
+++ Vaje/tretje število po velikosti.py
@@ -4,6 +4,8 @@ c = int(input("c= "))
 d = int(input("d= "))
 print(max((min(a,b),min(c,d))))
 
-
-        
+tuki je bla fora, da se reši samo z uporabo
+min max funkcij, zadeva da pravi rezultat, če 
+je višje število v prvi skupini višje od nižjega
+v drugi in obratno....
         
